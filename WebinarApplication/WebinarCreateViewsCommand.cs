f93c87747dc12189f90824da6d1622a3449c2b1a
MariyanSlesar <marianslesar.08@gmail.com>
2024-03-09 19:37:23 +0200

    First iteration

---
+++ WebinarApplication/WebinarCreateViewsCommand.cs
@@ -0,0 +1,35 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace WebinarApplication;
+
+[Transaction(TransactionMode.Manual)]
+[Regeneration(RegenerationOption.Manual)]
+public class WebinarCreateViewsCommand : IExternalCommand
+{
+    public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+    {
+        var uiApplication = commandData.Application;
+        var application = uiApplication.Application;
+        var uiDocument = uiApplication.ActiveUIDocument;
+        var document = uiDocument.Document;
+        var documentName = application.VersionNumber;
+        TaskDialog.Show("Title", $"Revit Version is : {documentName}");
+        return Result.Succeeded;
+    }
+}
+
+
+
+public interface IMyCommand
+{
+    void Execute();
+}
+public class MyCommand : IMyCommand
+{
+    public void Execute()
+    {
+        //here you would write actual implementation
+    }
+}
